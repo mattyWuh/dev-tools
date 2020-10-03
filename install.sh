115a84018ffcc521c3c23a67aa121cb1eddb6e47
mattyWuh <mattcwitt [at] gmail.com>
2020-10-03 09:28:21 -0700

    🔨 Use `bash` instead of  `./`

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ scripts=src/dev-tools
 sudo $pkg_mngr install curl build-essential libssl-dev libffi-dev -y
 
 # Set up font
-./$scripts/nerd_font.sh
+bash $scripts/nerd_font.sh
 
 # Set up shell
-sudo ./$scripts/zsh.sh $pkg_mngr
-./$scripts/starship.sh 
+sudo bash $scripts/zsh.sh $pkg_mngr
+bash $scripts/starship.sh 
 
 # Set up terminal
-./$scripts/hyper.sh
+bash $scripts/hyper.sh
 
 # Set up python
-./$scripts/python.sh $pkg_mngr
+bash $scripts/python.sh $pkg_mngr
 
 # Set up npm and associated packages
-./$scripts/node_npm.sh $pkg_mngr
+bash $scripts/node_npm.sh $pkg_mngr
